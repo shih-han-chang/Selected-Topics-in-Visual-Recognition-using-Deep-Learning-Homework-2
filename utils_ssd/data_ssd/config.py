7ea4fc6964f2469eeade4117274dac1df6787f12
shih-han-chang <74303716+shih-han-chang@users.noreply.github.com>
2020-11-26 22:17:16 +0800

    Add files via upload

---
+++ utils_ssd/data_ssd/config.py
@@ -0,0 +1,28 @@
+# config.py
+import os.path
+
+# gets home dir cross platform
+HOME = os.path.expanduser("~")
+
+# for making bounding boxes pretty
+COLORS = ((255, 0, 0, 128), (0, 255, 0, 128), (0, 0, 255, 128),
+          (0, 255, 255, 128), (255, 0, 255, 128), (255, 255, 0, 128))
+
+MEANS = (88, 95, 98)
+
+# SSD300 CONFIGS
+
+svhn = {
+    'num_classes': 11,
+    # 'lr_steps': (80000, 100000, 120000),
+    # 'max_iter': 120001,
+    'feature_maps': [38, 19, 10, 5, 3, 1],
+    'min_dim': 300,
+    'steps': [8, 16, 32, 64, 100, 300],
+    'min_sizes': [30, 60, 111, 162, 213, 264],
+    'max_sizes': [60, 111, 162, 213, 264, 315],
+    'aspect_ratios': [[2], [2, 3], [2, 3], [2, 3], [2], [2]],
+    'variance': [0.1, 0.2],
+    'clip': True,
+    'name': 'svhn',
+}
